00ace36688161e97e0cae9101518630ac44af397
Nitika4797 <161456956+Nitika4797@users.noreply.github.com>
2025-07-31 23:12:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
+console.log("loading/......");
   const bar = document.createElement("div");
-  bar.style.position = "fixed";
+
+ bar.style.position = "fixed";
   bar.style.top = "0";
   bar.style.left = "0";
   bar.style.width = "100%";
@@ -11,7 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
   bar.style.textAlign = "center";
   bar.style.fontSize = "16px";
   bar.style.zIndex = "9999";
-  bar.innerText = "Free Shipping on orders over $50 – Smart Shipping Bar Active!";
+ bar.innerText = "Free Shipping on orders over $50 – Smart Shipping Bar Active!";
 
+  // Add to page
   document.body.appendChild(bar);
 });
